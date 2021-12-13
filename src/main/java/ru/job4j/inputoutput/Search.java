a67092c113b1874051d94ad788f1affe0709b8da
vmyaskovskiy <slavav1977@yandex.ru>
2021-12-13 12:46:55 +0300

    5. Валидация параметров запуска. [#246865 #237850]new

--- src/main/java/ru/job4j/inputoutput/Search.java
+++ src/main/java/ru/job4j/inputoutput/Search.java
@@ -1,5 +1,6 @@
 package ru.job4j.inputoutput;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -16,8 +17,15 @@ public class Search {
     }
 
     public static void main(String[] args) throws IOException {
-        if (args.length == 0 || args.length != 2) {
-            throw new IllegalArgumentException("Root folder is null. Usage java -jar dir.jar ROOT_FOLDER.");
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Необходимо задать два аргумента");
+        }
+        File file = new File(args[0]);
+        if (!file.exists()) {
+            throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
+        }
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
         }
         Path test = Paths.get(args[0]);
         search(test, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
