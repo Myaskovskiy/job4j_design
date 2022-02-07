1bac10467edd4cdf67e03f90d82ecb5339ba3d15
vmyaskovskiy <slavav1977@yandex.ru>
2022-02-07 12:23:57 +0300

    0. Что такое база данных. [#6860] new

--- src/main/java/ru/job4j/serialization/selectCar.sql
+++ src/main/java/ru/job4j/serialization/selectCar.sql
@@ -1,16 +1,3 @@
-insert into car(name, description, TotalWeight, Cost, oldCar)
-values('BMW', 'пушка гонка', 100.2, 100500, true);
-
-insert into car(name, description, TotalWeight, Cost, oldCar)
-values('Porsche', 'супер стар', 200.5, 200500, false);
-
-update car set name = 'BMW-OLD', oldCar = false;
-
-delete from car;
-
-select * from car;
-select name from car where oldCar = true;
-
 create table car(
     id serial primary key,
 	name varchar(255),
@@ -19,3 +6,18 @@ create table car(
 	Cost INTEGER,
 	oldCar boolean
 );
+
+insert into car(name, description, TotalWeight, Cost, oldCar)
+values('BMW', 'luxery car', 100.2, 100500, true);
+
+insert into car(name, description, TotalWeight, Cost, oldCar)
+values('Porsche', 'sport car', 200.5, 200500, false);
+
+select * from car;
+select name from car where oldCar = true;
+
+update car set name = 'BMW-OLD', oldCar = false;
+
+delete from car;
+
+select * from car;
